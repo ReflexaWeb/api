95facc2e5c6f7e965525639cc95224a78f320414
Yan Soares <yts.1995@gmail.com>
2021-09-15 20:38:42 -0300

    feat: add get-products controller and tests

---
+++ src/controllers/get-products.ts
@@ -0,0 +1,15 @@
+import { GetAllProduct } from '@/domain/contracts/repos'
+import { Request, Response } from 'express'
+
+export class GetProductsController {
+  constructor (private readonly productUsecase: GetAllProduct) {}
+
+  async handle (_: Request, res: Response): Promise<void> {
+    try {
+      const products = await this.productUsecase.getAll()
+      res.status(200).json(products)
+    } catch (error) {
+      res.status(500).json({ message: error })
+    }
+  }
+}
---
+++ tests/controllers/get-products.spec.ts
@@ -0,0 +1,61 @@
+import { GetProductsController } from '@/controllers'
+import { GetAllProduct } from '@/domain/contracts/repos'
+import { productData } from '@/tests/domain/mocks'
+
+import { mock, MockProxy } from 'jest-mock-extended'
+import { Request, Response } from 'express'
+
+describe('GetProductsController', () => {
+  let productRepo: MockProxy<GetAllProduct>
+  let req: Request
+  let res: Response
+  let sut: GetProductsController
+
+  beforeAll(() => {
+    productRepo = mock()
+    req = mock()
+    res = mock()
+
+    res.status = jest.fn().mockReturnThis()
+    res.json = jest.fn().mockReturnThis()
+
+    productRepo.getAll.mockResolvedValue([productData])
+  })
+
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
+
+  beforeEach(() => {
+    sut = new GetProductsController(productRepo)
+  })
+
+  it('should return 200 with data', async () => {
+    await sut.handle(req, res)
+
+    expect(productRepo.getAll).toHaveBeenCalled()
+    expect(res.json).toHaveBeenCalledWith([productData])
+    expect(res.status).toHaveBeenCalledWith(200)
+  })
+
+  it('should return 200 without data', async () => {
+    productRepo.getAll.mockResolvedValueOnce([])
+
+    await sut.handle(req, res)
+
+    expect(productRepo.getAll).toHaveBeenCalled()
+    expect(res.json).toHaveBeenCalledWith([])
+    expect(res.status).toHaveBeenCalledWith(200)
+  })
+
+  it('should return 500', async () => {
+    const error = new Error('some error')
+    productRepo.getAll.mockRejectedValueOnce(error)
+
+    await sut.handle(req, res)
+
+    expect(productRepo.getAll).toHaveBeenCalled()
+    expect(res.status).toHaveBeenCalledWith(500)
+    expect(res.json).toHaveBeenCalledWith({ message: error })
+  })
+})
