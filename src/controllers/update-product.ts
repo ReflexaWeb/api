9bdc6af5e7249c9604cb1a7d80c274a509416cf0
Yan Soares <yts.1995@gmail.com>
2021-09-15 20:37:32 -0300

    feat: add update-product controller and tests

---
+++ src/controllers/update-product.ts
@@ -0,0 +1,25 @@
+import { UpdateProduct } from '@/domain/contracts/repos'
+import { ProductNotFound } from '@/errors'
+
+import { Request, Response } from 'express'
+
+export class UpdateProductController {
+  constructor (private readonly productUsecase: UpdateProduct) {}
+
+  async handle (req: Request, res: Response): Promise<void> {
+    try {
+      await this.productUsecase.update(req.params.code, req.body)
+      res.sendStatus(200)
+    } catch (error) {
+      this.handleError(error, res)
+    }
+  }
+
+  private handleError (error: any, res: Response): void {
+    if (error instanceof ProductNotFound) {
+      res.status(422).json({ message: error.message })
+    } else {
+      res.status(500).json({ message: error })
+    }
+  }
+}
---
+++ tests/controllers/update-product.spec.ts
@@ -0,0 +1,67 @@
+import { UpdateProductController } from '@/controllers'
+import { GetProductByCode, UpdateProduct } from '@/domain/contracts/repos'
+import { productData } from '@/tests/domain/mocks'
+
+import { mock, MockProxy } from 'jest-mock-extended'
+import { Request, Response } from 'express'
+import { ProductNotFound } from '@/errors'
+
+describe('UpdateProductController', () => {
+  let productUsecase: MockProxy<UpdateProduct & GetProductByCode>
+  let req: Request
+  let res: Response
+  let sut: UpdateProductController
+
+  beforeAll(() => {
+    productUsecase = mock()
+    req = mock()
+    res = mock()
+
+    res.sendStatus = jest.fn().mockReturnThis()
+    res.status = jest.fn().mockReturnThis()
+    res.json = jest.fn().mockReturnThis()
+  })
+
+  afterEach(() => {
+    jest.clearAllMocks()
+
+    req.body = {}
+    req.params = {}
+  })
+
+  beforeEach(() => {
+    sut = new UpdateProductController(productUsecase)
+
+    req.body = { ...productData }
+    req.params = { code: productData.code }
+  })
+
+  it('should return 200', async () => {
+    await sut.handle(req, res)
+
+    expect(productUsecase.update).toHaveBeenCalledWith(req.params.code, { ...req.body })
+    expect(res.sendStatus).toHaveBeenCalledWith(200)
+  })
+
+  it('should return 422', async () => {
+    const mockThrownError = new ProductNotFound('some error')
+    productUsecase.update.mockRejectedValueOnce(mockThrownError)
+
+    await sut.handle(req, res)
+
+    expect(productUsecase.update).toHaveBeenCalledWith(req.params.code, { ...req.body })
+    expect(res.status).toHaveBeenCalledWith(422)
+    expect(res.json).toHaveBeenCalledWith({ message: mockThrownError.message })
+  })
+
+  it('should return 500', async () => {
+    const mockThrownError = new Error('some error')
+    productUsecase.update.mockRejectedValueOnce(mockThrownError)
+
+    await sut.handle(req, res)
+
+    expect(productUsecase.update).toHaveBeenCalledWith(req.params.code, { ...req.body })
+    expect(res.status).toHaveBeenCalledWith(500)
+    expect(res.json).toHaveBeenCalledWith({ message: mockThrownError })
+  })
+})
