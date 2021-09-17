3ef018a54776555271b376f387a6325e1ee693fa
Yan Soares <yts.1995@gmail.com>
2021-09-16 21:43:24 -0300

    chore: update db connection example

--- ormconfig-local.js
+++ ormconfig-local.js
@@ -1,11 +1,12 @@
 module.exports = {
   name: 'default',
   type: 'mysql',
-  host: 'localhost',
+  host: process.env.MYSQL_HOST,
   port: process.env.MYSQL_PORT,
   username: process.env.MYSQL_USERNAME,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
+  migrations: ['./src/infra/db/mysql/migrations/*.ts'],
   entities: ['./src/infra/db/mysql/entities/*.ts'],
   cli: {
     migrationsDir: './src/infra/db/mysql/migrations/*.ts'
