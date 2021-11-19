d46e22f85098b54ac8ba4522a5aeaf69ff59dcb9
Yan Soares <yts.1995@gmail.com>
2021-11-19 10:04:19 -0300

    chore: cors

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,9 +1,7 @@
-import { setupMiddlewares } from '@/main/config/middlewares'
 import { setupRoutes } from '@/main/config/routes'
 
 import express from 'express'
 
 export const app = express()
 
-setupMiddlewares(app)
 setupRoutes(app)
--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -1,6 +1,7 @@
-import { Express, Router } from 'express'
+import { Express, Router, json } from 'express'
 import { readdirSync } from 'fs'
 import { join, resolve } from 'path'
+import cors from 'cors'
 import { serve, setup } from 'swagger-ui-express'
 import YAML from 'yamljs'
 
@@ -13,6 +14,8 @@ export const setupRoutes = (app: Express): void => {
 
   const swaggerDocument = YAML.load(resolve(__dirname, '../../../api-spec.yaml'))
 
+  app.use(cors())
+  app.use(json())
   app.use('/api/v1', router)
   app.use('/api/docs', serve, setup(swaggerDocument))
 }
