beb76338a2b5b73f422689ae91ed15d06fe22b5b
Yan Soares <yts.1995@gmail.com>
2022-06-04 12:18:40 -0300

    chore: add cors origin and npm scripts

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -17,7 +17,9 @@ export const setupRoutes = (app: Express): void => {
 
   const swaggerDocument = YAML.load(resolve(__dirname, '../../../api-spec.yaml'))
 
-  app.use(cors())
+  app.use(cors({
+    origin: 'https://www.reflexa.com.br/'
+  }))
   app.use(json())
   app.use(pagination)
   app.use(limiter)
