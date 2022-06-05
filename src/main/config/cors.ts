4319740888fd4583418822ccc4cb2a314a72f970
Yan Soares <yts.1995@gmail.com>
2022-06-05 11:04:21 -0300

    chore: reverting original cors

--- src/main/config/cors.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response, NextFunction } from 'express'
-
-export const cors = (_: Request, response: Response, next: NextFunction): void => {
-  response.set('access-control-allow-origin', 'https://www.reflexa.com.br/')
-  response.set('Access-Control-Allow-Methods', ['GET', 'PUT', 'POST'])
-  next()
-}
