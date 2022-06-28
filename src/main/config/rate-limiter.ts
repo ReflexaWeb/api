f6c6f1ba3ceae79461f605e5f4c02ea676bbc9f1
Yan Soares <yts.1995@gmail.com>
2022-06-28 13:53:59 -0300

    chore: increase rate-limiter max requests

--- src/main/config/rate-limiter.ts
+++ src/main/config/rate-limiter.ts
@@ -1,7 +1,7 @@
 import rateLimit from 'express-rate-limit'
 
 const ONE_MINUTE = 1 * 60 * 1000
-const MAX_REQUESTS = 50
+const MAX_REQUESTS = 1000
 
 export const limiter = rateLimit({
   windowMs: ONE_MINUTE,
